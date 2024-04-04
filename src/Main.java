39da8ac36e61953c9463d69b5a98e46d1cadaefe
Eloise2011 <joshuahbrooks@163.com>
2024-04-04 17:47:47 +0800

    5th commit

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 /**
- * @description 
+ * @author Joshua.H.Brooks
+ * @description
  * @date 2024-04-04 16:10
- * @author Joshua.H.Brooks 
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello Git!");
     }
 }
