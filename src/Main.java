bc9959bbf77e7b594bbebf1d38c88d65dfc3cef8
Eloise2011 <joshuahbrooks@163.com>
2024-04-04 17:43:14 +0800

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * @description 
+ * @date 2024-04-04 16:10
+ * @author Joshua.H.Brooks 
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
